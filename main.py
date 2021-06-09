5b94e35a4df544cbab5eb1eb296edc0ee433ae40
AbeTavarez <abeincwireless@gmail.com>
2021-06-08 21:13:58 -0400

    connection done

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
+from os import close
 import sqlite3
 from sqlite3.dbapi2 import connect
 
+# creates a DB connection
 connection = sqlite3.connect('test_database.db')
 
-print(type(connection))
+# Creates a Cursor
+cursor = connection.cursor()
+
+# Creates a Query
+query = "SELECT datetime('now', 'localtime');"
+
+# Executing the Query
+result = cursor.execute(query)
+print(result)
+
+# Fetches the first row of results
+row = result.fetchone() # returns a tuple
+print(row) 
+
+# Access the time from the tuple
+time = row[0] # selects the first item (in this case the only item) in the tuple 
+print(time)
+
+
+# Close DB Connection
+connection.close()
