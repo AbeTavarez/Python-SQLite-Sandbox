e1959eb46fec74997ef55efd9937df88ab218eba
AbeTavarez <abeincwireless@gmail.com>
2021-06-09 23:23:45 -0400

    sql script

--- SQL-Script/running-sql-scripts.py
+++ SQL-Script/running-sql-scripts.py
@@ -1,8 +1,12 @@
+# ================================================
+# Creates database and inserts data using a script
+# ================================================
 # A SQL Script is a collection of SQL statements separated by semicolons(:)
 # That can be run all at the same time.
 
 import sqlite3
 
+# Creating script....
 sql = """
 DROP TABLE IF EXISTS People;
 CREATE TABLE People(
@@ -15,4 +19,11 @@ INSERTE INTO People VALUES(
     'Yaeger',
     '33'
 );"""
+
+
+# Executing....
+with sqlite3.connect('people_database.db') as connection:
+    cursor = connection.cursor()
+    cursor.executescript(sql)
+
 
